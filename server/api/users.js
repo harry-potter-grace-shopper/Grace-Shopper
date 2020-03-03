e8d5fd916aee8be541d0a2629a9ef35313bf6a2f
Yumiko <yumikomannarelli@gmail.com>
2020-03-03 17:31:49 -0500

    user routes

--- server/api/users.js
+++ server/api/users.js
@@ -1,8 +1,37 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const {User, Product, Cart, Order} = require('../db/models')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+const adminsOnly = (req, res, next) => {
+  if (!req.user.admin) {
+    const err = new Error("Wait, that's illegal")
+    err.status = 401
+    return next(err)
+  }
+  next()
+}
+
+const currentUserOnly = (req, res, next) => {
+  if (req.user.id !== Number(req.params.userId)) {
+    const err = new Error("Wait, that's illegal")
+    err.status = 401
+    return next(err)
+  }
+  next()
+}
+
+const adminOrCurrentUser = (req, res, next) => {
+  if (req.user.id === Number(req.params.userId) || req.user.admin) {
+    next()
+  } else {
+    const err = new Error('Not your Page!')
+    err.status = 401
+    return next(err)
+  }
+}
+
+//get all users for admin only
+router.get('/', adminsOnly, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
@@ -15,3 +44,68 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+//get single page user for admins and the logged in user
+router.get('/:userId', adminOrCurrentUser, async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId)
+    if (user) {
+      res.status(200).json(user)
+    } else {
+      res.status(404).json('User is not defined!')
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+// create  and undo user/product association in  cart +
+
+router.get(
+  '/:action/:userId/:prodId',
+  currentUserOnly,
+  async (req, res, next) => {
+    try {
+      const user = await User.findByPk(req.params.userId)
+      const product = await Product.findByPk(req.params.prodId)
+      if (req.params.action === 'add') {
+        user.addProduct(product)
+        res.send(product)
+      } else {
+        user.removeProduct(product)
+        res.sendStatus(204)
+      }
+      //this sends back the newly added product to then add to the state
+    } catch (err) {
+      next(err)
+    }
+  }
+)
+
+router.get('/checkout/:userId', currentUserOnly, async (req, res, next) => {
+  try {
+    const cartItems = await Cart.findAll({
+      where: {
+        userId: req.params.userId
+      },
+      attributes: ['productId']
+    })
+
+    // WE WILL NEED TO UPDATE THIS TO INCLUDE THE ADDITIONAL FORM DATA FOR ORDER PROPERTIES
+    //RIGHT NOW IT IS ONLY SENDING PRODUCTIDs
+    console.log(cartItems)
+    const orderItems = cartItems.map(obj => {
+      return obj.productId
+    })
+    const newOrder = await Order.create({
+      productsId: orderItems,
+      paymentMethod: 'paypal',
+      shippingInfo: 'home'
+    })
+    res.json(newOrder)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//
