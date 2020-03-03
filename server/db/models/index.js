0bd512fd020c55fcb1ebc7eab420f82e3203bda4
Yumiko <yumikomannarelli@gmail.com>
2020-03-03 11:28:54 -0500

    created product model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,5 @@
 const User = require('./user')
+const Product = require('./product')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -7,6 +8,7 @@ const User = require('./user')
  *    BlogPost.belongsTo(User)
  */
 
+User.belongsToMany(Product,{ through: 'cart'})
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -14,5 +16,6 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User,
+  Product
 }
