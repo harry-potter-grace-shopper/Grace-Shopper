b25ca0dad66a0e112c30e54ff71d17be8af76f00
Sarah Zhao <sarahzhao25@gmail.com>
2020-03-04 12:54:36 -0500

    added comments

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -6,8 +6,13 @@ const Cart = db.define('cart', {
     type: Sequelize.INTEGER,
     defaultValue: 1
   }
+  // need a price field here to record the CURRENT price when it was PURCHASED/when you added it
 })
 
+// I would suggest making this a CLASS method and not an INSTANCE method
+// you would need the cartId, perform a findone on cart, and then call changeCart
+// Cart.changeChart
+// Cart.prototype.changeCart = this = cartItem
 Cart.prototype.changeCart = async function(userId, itemId, action) {
   const cartItem = await Cart.findOne({
     where: {userId: userId, productId: itemId}
--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,13 +2,15 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
+  // many to many relationship with products, you don't need the duplicate products array here to store all of the ids -> you can use a magic method order.getProducts() to retrieve all of those products anyway.
   productsId: {
     type: Sequelize.ARRAY(Sequelize.INTEGER)
   },
   totalCost: {
-    type: Sequelize.DECIMAL
-  },
+    type: Sequelize.DECIMAL // int
+  }, // would also consider looking into a virtual if you want to access this as a property OR use a prototype method in order to build this.
   paymentMethod: {
+    // i also think -> you may not need this information in here because stripe/paypal will deal with it for you
     type: Sequelize.ENUM('paypal', 'stripe', 'creditCard'),
     allowNull: false
   },
