1c2953a67023f7dc58172447b3cee47c856b1f48
Lilly Oh <ohlilly@gmail.com>
2020-03-09 00:25:16 -0400

    updates to navbar, checkout form

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -24,7 +24,7 @@ const Navbar = ({handleClick, isLoggedIn, user}) => (
             <Link to="/home">Home</Link>
             {!user.admin ? <Link to={`/${user.id}/cart`}>Cart</Link> : <span />}
             {user.admin ? <Link to="/users">Users</Link> : <span />}
-            <a href="#" onClick={handleClick}>
+            <a href="/home" onClick={handleClick}>
               Logout
             </a>
           </div>
