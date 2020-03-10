d181722241d65db15e82fadda55f04603d3afc92
Lilly Oh <ohlilly@gmail.com>
2020-03-10 12:40:42 -0400

    user profile update

--- client/components/OrderHistory.js
+++ client/components/OrderHistory.js
@@ -12,34 +12,43 @@ class OrderHistory extends React.Component {
   render() {
     const {orders} = this.props
     return (
-      <div className="order-history-page">
-        <h2>My Orders</h2>
-        <ol className="order-history-container">
-          {orders.map(order => {
-            return (
-              <li key={order.id} className="order-history-item">
-                <p>Order Date: {order.products[0].order_history.date}</p>
-                {order.products.map(item => {
-                  return (
-                    <div key={item.id}>
-                      <Link to={`/products/${item.id}`}>
-                        <img
-                          className="order-history-img"
-                          src={item.imageUrl}
-                          width="100px"
-                        />
-                      </Link>
-                      <div className="order-history-details">
-                        <p>Quantity: {item.order_history.quantity}</p>
-                        <p>Price: ${item.order_history.currentPrice}.00</p>
+      <div className="page-container">
+        <div>
+          <h1>Account Overview</h1>
+          <p>Name: </p>
+          <p>Email: </p>
+        </div>
+
+        <div className="order-history-page">
+          <h2>My Orders</h2>
+
+          <ol className="order-history-container">
+            {orders.map(order => {
+              return (
+                <li key={order.id} className="order-history-item">
+                  <p>Order Date: {order.products[0].order_history.date}</p>
+                  {order.products.map(item => {
+                    return (
+                      <div key={item.id}>
+                        <Link to={`/products/${item.id}`}>
+                          <img
+                            className="order-history-img"
+                            src={item.imageUrl}
+                            width="100px"
+                          />
+                        </Link>
+                        <div className="order-history-details">
+                          <p>Quantity: {item.order_history.quantity}</p>
+                          <p>Price: ${item.order_history.currentPrice}.00</p>
+                        </div>
                       </div>
-                    </div>
-                  )
-                })}
-              </li>
-            )
-          })}
-        </ol>
+                    )
+                  })}
+                </li>
+              )
+            })}
+          </ol>
+        </div>
       </div>
     )
   }
