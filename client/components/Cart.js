391c1e7f44ca595248a2fce8021f5020aadf1ce5
Aleksandra Bardymova <bardymovasasha@gmail.com>
2020-03-10 12:33:51 -0400

    can merge guest and user carts

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -41,7 +41,6 @@ class Cart extends React.Component {
 
   render() {
     const products = this.props.products
-    console.log(products)
     if (products.length === 0)
       return (
         <div className="cart-page">
